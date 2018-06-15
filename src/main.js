7646a5a1863d24350855f9ba9071d275307b584e
uynet <highsate@gmail.com>
2018-06-15 10:19:09 +0900

    サイドバー開くようになった

--- src/main.js
+++ src/main.js
@@ -1,38 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<div class="box">{{ todo.text }}</div>'
-})
-
 new Vue({
   el:"#hambar",
   data:{
-    view:"v-a",
-    count:0,
-  },
-  components:{
-    'v-a': {
-      template: '<div class="unko">A</div>'
-    },
-    'v-b': {
-      template: '<div>B</div>'
-    },
+    lass:"sidebar",
   },
   methods:{
     po:function(){
-      this.count++;
-      if(this.view == "v-a")this.view = "v-b";
-      else if (this.view == "v-b")this.view = "v-a";
-      console.log(this.count);
+      this.lass = "sidebar_open";
+      console.log(this.lass)
     }
-  },
-});
-new Vue({
-  el: '#demo',
-  data: {
-    show: true
   }
 })
