aa313381e524a38290e117f7020dba51ec5f9310
uynet <highsate@gmail.com>
2018-06-16 04:14:41 +0900

    darkness

--- src/main.js
+++ src/main.js
@@ -1,21 +1,52 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.component('item-menu', {
+  props:["title","open"],
+  template:`
+  <div v-on:click="open">{{title}}<div class="label"></div></div>
+  `
+});
+
 new Vue({
-  el:"#hambar",
+  el:"#page",
   data:{
-    lass:"sidebar",
+    sidebar:"sidebar",
+    container:"container",
+    layer:"layer",
     items:[
-      {message:"about"},
-      {message:"works"},
-      {message:"unko"},
+      {
+        message:"About",
+        open:function(){
+          console.log("po");
+        }
+      },
+      {
+        message:"Works",
+        open:function(){
+          console.log("po");
+        }
+      },
+      {
+        message:"Unko",
+        open:function(){
+          console.log("po");
+        }
+      },
     ],
   },
   methods:{
-    po:function(){
-      if(this.lass=="sidebar")this.lass = "sidebar_open";
-      else this.lass = "sidebar";
-    }
+    slide:function(){
+      console.log(this.layer)
+      if(this.sidebar=="sidebar"){
+        this.sidebar = "sidebar_open";
+        this.layer = "layer_dark";
+      }
+      else{
+        this.sidebar = "sidebar";
+        this.layer = "layer";
+      }
+    },
   }
 })
 
