9b5e2dfbe91ae10e78eb77ebb8e1fdaab3f7b54b
uynet <highsate@gmail.com>
2018-06-26 04:35:10 +0900

    addworks

--- src/Works/works.vue
+++ src/Works/works.vue
@@ -6,6 +6,37 @@
 
 <containts v-bind:products="this.products"></containts>
 
+<div align="center" class="works">音楽</div>
+
+<div align="center">
+<iframe style="border: 0; width: 100%; height: 112px;" src="https://bandcamp.com/EmbeddedPlayer/album=1352470804/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=2081494030/transparent=true/" seamless><a href="http://trap-titech.bandcamp.com/album/trap-2018"]traP新歓コンピ2018 by uynet</a></iframe>
+
+<iframe width="100%" height="128" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/saihate-1/immortal-flower&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"></iframe>
+
+部内音ゲーに提供
+
+<iframe width="100%" height="128" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/saihate-1/ninjsflicker-ost&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"></iframe>
+
+NinjaFlicker
+
+<iframe width="100%" height="128" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=
+https://soundcloud.com/saihate-1/nmqlbynuqwaf
+&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"></iframe>
+
+NinjaFlicker ステージ曲
+
+<iframe width="100%" height="128" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/saihate-1/pobattle4&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"></iframe>
+ポ
+
+<iframe width="100%" height="128" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/saihate-1/2017m3preview&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"></iframe>
+
+2017秋M3
+
+<iframe width="100%" height="128" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/saihate-1/rainbow-treasure&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"></iframe>
+
+無料音源で本気だした
+</div>
+
 </div>
 </template>
 <script>
--- src/about.vue
+++ src/about.vue
@@ -1,6 +1,12 @@
 <template>
-
+<div>
 <router-link to="/">
-<div align="center">あばうとだよ</div>
 </router-link>
+<div class="passage" align="center">
+uynet/ゆいねっと(ユイ)<br>
+所属:東京工業大学情報工学科B4<br>
+/<a href="http://trap.jp" class="popo">デジタル創作同好会traP</a><br>
+いろいろやる
+</div>
+</div>
 </template>
