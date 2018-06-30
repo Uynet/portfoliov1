4ac7815d56f1fae75a45e89080a2146294f71303
uynet <highsate@gmail.com>
2018-07-01 00:05:23 +0900

    prof

--- src/about.vue
+++ src/about.vue
@@ -2,15 +2,30 @@
 <div>
 <div align="center">
 <h3>ぷろふぃーる</h3>
+<div class="image">
+<img src="../resource/chara.png" align="center"></img>
+</div>
 <div class="frame" align="center" style="max-width:400px">
-<div class="passage" style="margin-left:32px">
-<br><br><br>
+<div class="passage" >
+<h4>name</h4>
 uynet/ゆい<br>
 所属:東京工業大学情報工学科B4<br>
 <a href="http://trap.jp" style="color:#3394b8">デジタル創作同好会traP</a><br>
-<br>
+birth:1996/12/19
+<h4>すき</h4>
+ピアノ<br>
+作曲(DTM)<br>
+ドット絵<br>
+ゲーム制作<br>
+<h4>言語</h4>
+javascript<br>
+webGL<br>
+vueJS<br>
+C++<br>
+Java
+<h4>リンク</h4>
+twitter/github/soundcloud
 </div>
-<img src="../resource/chara.png" align="center"></img>
 </div>
 </div>
 </div>
