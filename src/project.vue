da7cd39abb870687a21962ea32f7980ac2e765f0
uynet <highsate@gmail.com>
2018-06-30 19:53:24 +0900

    modify

--- src/project.vue
+++ src/project.vue
@@ -1,3 +1,19 @@
 <template>
-<h3>うんち</h3>
+<div>
+<h3>かかわったもの</h3>
+<div class="frame">
+<passage>NinjaFlicker</passage>
+</div>
+
+<div class="frame">
+<passage>po</passage>
+</div>
+
+<div class="frame">
+<passage>ninja's revenge</passage>
+</div>
+
+</div>
 </template>
+<script>
+</script>
