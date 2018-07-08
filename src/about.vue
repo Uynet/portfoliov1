7db2a7535a8b03247ce70564406aecaed0d938c2
uynet <highsate@gmail.com>
2018-07-09 01:55:42 +0900

    サイズ調整

--- src/about.vue
+++ src/about.vue
@@ -8,24 +8,19 @@
 </div>
 <div class="passage" >
 uynet/ゆい<br>
+🎂 1996/12/19<br>
 所属:東京工業大学情報工学科B4<br>
 <a href="http://trap.jp" >デジタル創作同好会traP</a><br>
-birth:1996/12/19
 <h4>趣味</h4>
-<a href="https://twitter.com/uynet/status/846618194513031168">ピアノ</a> ... 中学生のときからやっています(独学)<br>
-<a href="https://twitter.com/highsate/status/983628562870644736">作曲(DTM)</a> ... 上に並行してはじめました<br>
-Cubase7.5/QLSO/Sylenth1,<br>
-Pops/Artcore/Orchestra/Chiptune/...<br>
-ゲーム制作 ... サークルor個人でやります<br>
-ドット絵 ... ゲーム用<br>
+<a href="https://twitter.com/uynet/status/846618194513031168">ピアノ</a><br>
+<a href="https://twitter.com/highsate/status/983628562870644736">DTM</a><br>
+ゲーム制作<br>
+ドット絵/イラスト<br>
 <h4>言語</h4>
 JavaScript,webGL,vueJS,HTML,CSS<br>
 C++,Java<br>
-エディタはvim<br>
-<h4>ソーシャル</h4>
-twitter : @highsate / @uynet<br>
-github @uynet<br>
-soundcloud<br>
+<h4>エディタ</h4>
+vim<br>
 </div>
 </div>
 </div>
